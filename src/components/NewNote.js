2473a14cfb962d4654025c25bf6afa4896f777aa
Ramon Agud <agudadrian92@gmail.com>
2022-10-14 17:51:24 +0300

    Never doing these mistakes again

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -2,13 +2,32 @@ import styles from "styles/home.module.css"
 
 // MUI Imports
 import Button from '@mui/material/Button'
+import { useState } from "react"
 
-const NewNote = () => {
+const NewNote = (props) => {
+    const [message, setMessage] = useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("sending", JSON.stringify({message: message}))
+
+        fetch("http://localhost:9001/api/sticky-notes", {
+            method: "POST",
+            body: JSON.stringify({message: message}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => { 
+                console.log(response)
+                setMessage("")
+            })
+    }
     return (
         <div className={styles.newNote}>
-            <form method="post">
-                <textarea name="content" rows="3" cols="20" placeholder="Note content / text"></textarea>
-                <Button variant="contained">Create new note</Button>
+            <form method="POST" onSubmit={event => handleSubmit(event)}>
+                {/* <textarea name="content" rows="3" cols="20" placeholder="Note content / text"></textarea> */}
+                <input type="text" placeholder="New note text" name="message" onChange={event => setMessage(event.target.value)} />
+                <Button variant="contained" type="submit">Create new note</Button>
             </form>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,19 @@ import Head from 'next/head'
 import Note from 'components/Note'
 import NewNote from 'components/NewNote'
 import styles from 'styles/home.module.css'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
   var tempContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:9001/api/sticky-notes")
+      .then((response) => response.json())
+      .then((data) => setNotes(data))
+   }, []);
+  const stickyNotes = notes.map(
+    note => <Note content={note.message} rank={note.rank} />
+  )
   return (
     <>
       <Head>
@@ -13,13 +23,7 @@ export default function Home() {
       <div className={styles.header}>
         <h1>Whiteboard</h1>
         <div className={styles.whiteboardContainer}>
-          <Note content={tempContent} rank={3} />
-          <Note content={tempContent} rank={3} />
-          <Note content={tempContent} rank={3} />
-          <Note content={tempContent} rank={3} />
-          <Note content={tempContent} rank={3} />
-          <Note content={tempContent} rank={3} />
-          <Note content={tempContent} rank={3} />
+          {stickyNotes}
           <NewNote />
         </div>
       </div>
